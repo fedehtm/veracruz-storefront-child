2fb8e662983332e3069a4e0cafcf96707bf5efd7
Federico <25438748+fedehtm@users.noreply.github.com>
2019-02-09 11:22:27 -0300

    Schema Markup Principal
    Agregar datos estructurados a la página principal

--- functions.php
+++ functions.php
@@ -36,4 +36,44 @@ function disable_yoast_schema_data($data){
 	return $data;
 }
 add_filter('wpseo_json_ld_output', 'disable_yoast_schema_data', 10, 1);
+
+add_action('wp_head', 'schema_home');
+function schema_home(){
+if(is_front_page()) {  ?>
+	<script type="application/ld+json">
+	{
+	  "@context": "http://schema.org",
+	  "@type": "LocalBusiness",
+	  "name": "Vera Cruz | Insumos Cerveceros",
+	  "description": "Materias primas para cerveza artesanal.",
+	  "logo": "https://www.veracruzinsumos.com.ar/wp-content/uploads/Logo-Schema-Markup.jpg",
+	  "image": "https://www.veracruzinsumos.com.ar/wp-content/uploads/Frente-Schema-Markup.jpg",
+	  "url": "https://www.veracruzinsumos.com.ar/",
+	  "sameAs": ["https://www.facebook.com/insumosveracruz/"],
+	  "openingHours": "Mo-Fr 08:00-17:00",
+	  "address":
+	  {
+	  "@type": "PostalAddress",
+	  "streetAddress": "Estanislao Zeballos 3621",
+	  "addressLocality": "Santa Fe",
+	  "addressRegion": "Santa Fe",
+	  "addressCountry": "Argentina"
+	  },
+	  "geo": {
+		"@type": "GeoCoordinates",
+		"latitude": "-31.602652",
+		"longitude": "-60.707833"
+	  },
+	  "aggregateRating": {
+		"@type": "AggregateRating",
+		"bestRating": "5",
+		"ratingValue": "4.0",
+		"reviewCount": "68"
+	  },
+	  "priceRange": "$$$",
+	  "telephone": "+54-0342-484-8642"
+	}
+	</script>
+<?php  }
+};
 ?>
